9f95ce392272b6bb6d9e104e143b5993d76ff355
ajoy267 <andrew.joy12@hotmail.com>
2021-12-16 11:48:56 -0800

    got homepage link to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import BookList from './views/Books/BookList';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import Home from './views/Books/Home/Home';
 import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/books">
+            <header>
+              <NavLink to="/" data-testid="/" exact>
+                Back to Home Page
+              </NavLink>
+            </header>
+            <BookList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
